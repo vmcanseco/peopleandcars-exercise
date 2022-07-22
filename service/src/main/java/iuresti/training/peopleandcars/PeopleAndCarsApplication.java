f1d17e3dcb25ddec5a094850a3fe1b5734954f89
Ivan Uresti <iuresti@skytouchtechnology.com>
2022-07-22 02:14:14 -0500

    Base files for exercise

---
+++ service/src/main/java/iuresti/training/peopleandcars/PeopleAndCarsApplication.java
@@ -0,0 +1,13 @@
+package iuresti.training.peopleandcars;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeopleAndCarsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PeopleAndCarsApplication.class, args);
+	}
+
+}
